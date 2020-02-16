b3180dffa9b6574c3d012e8881d128f80779886e
brennannicholson <57183331+brennannicholson@users.noreply.github.com>
2020-02-16 20:36:30 +0100

    Check whether unzip is installed before trying to use it.

--- data_prep/greek_data_prep/download_data.py
+++ data_prep/greek_data_prep/download_data.py
@@ -1,5 +1,20 @@
 """Downloads the Perseus and F1KG files. It should work with other versions of the Perseus and F1KG, but for the sake of reproducibility the specific versions used during development are downloaded."""
 import subprocess
+import sys
+
+
+# test whether unzip is installed
+def check_unzip():
+    try:
+        subprocess.run(
+            ["unzip"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
+        )
+    except FileNotFoundError:
+        print(
+            "unzip is required but not installed. Try installing it with 'sudo apt-get install unzip' and then run this script again.",
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
 
 def download_git_repo(address, commit):
@@ -15,6 +30,7 @@ def download(address):
 
 
 def get_data():
+    check_unzip()
     perseus_repo = "https://github.com/PerseusDL/canonical-greekLit"
     perseus_commit = "803db1425219cd300d907e19c2cb958e6bd5cbbd"
     f1kg_address = "https://zenodo.org/record/2592513/files/OpenGreekAndLatin/First1KGreek-1.1.4529.zip"
--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# check whether unzip is installed
+
+if ! command -v unzip >/dev/null 2>&1; then
+	echo >&2 "unzip is required but not installed. Try installing it with 'sudo apt-get install unzip' and then run this script again."
+	exit 1
+fi
 curl https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-models/research/finetuned_pythia_greek_char_BERT.zip > finetuned_pythia_greek_char_BERT.zip
 curl https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-models/research/greek_char_BERT.zip > greek_char_BERT.zip
 unzip finetuned_pythia_greek_char_BERT.zip -d ./models/finetuned_pythia_greek_char_BERT/
